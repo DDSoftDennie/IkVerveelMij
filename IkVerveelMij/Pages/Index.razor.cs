a5f2d46a1ed453682766b85658150a5d3f51210f
Dennie Declercq <dennie@ddsoft.be>
2021-07-14 17:22:41 +0200

    code cleanup

--- IkVerveelMij/Pages/Index.razor.cs
+++ IkVerveelMij/Pages/Index.razor.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using IkVerveelMij.Model;
+﻿using IkVerveelMij.Model;
 using Microsoft.AspNetCore.Components;
 
 namespace IkVerveelMij.Pages
@@ -12,12 +8,11 @@ namespace IkVerveelMij.Pages
         private int _count;
         public MarkupString ContentToShow { get; set; }
 
-        public void  Next()
+        public void Next()
         {
-            _count = _count < Content.Count-1 ? _count += 1 : 0;
+            _count = _count < Content.Count - 1 ? _count += 1 : 0;
             ContentToShow = Content.GetContent(_count);
         }
-
         protected override void OnInitialized()
         {
             base.OnInitialized();
