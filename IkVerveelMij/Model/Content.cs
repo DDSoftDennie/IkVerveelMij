0efe7c59dcc2cd0fd2cc4aa4082fb8809c976348
Dennie <dennie@ddsoft.be>
2021-07-14 19:34:48 +0200

    Typo in Content.cs

--- IkVerveelMij/Model/Content.cs
+++ IkVerveelMij/Model/Content.cs
@@ -10,7 +10,7 @@ namespace IkVerveelMij.Model
         {
             new MarkupString("Ik heb tijd vrij..."),
             new MarkupString("Wat zou ik doen?"),
-            new MarkupString("Laat ik een: <br /><ul><li>Niew boek</li><li>Nieuwe trui</li><li>Uitstapje</li><li>...</li></ul>leveren!<br/> <strong>Super</strong>! <br/> En als we terwijl aan <strong>DDSoft vzw</strong> denken..."),
+            new MarkupString("Laat ik een: <br /><ul><li>Nieuw boek</li><li>Nieuwe trui</li><li>Uitstapje</li><li>...</li></ul>leveren!<br/> <strong>Super</strong>! <br/> En als we terwijl aan <strong>DDSoft vzw</strong> denken..."),
             new MarkupString("Dan bestellen we bij <strong>TROOPER!</strong> <br/> WANT..."),
             new MarkupString("Het kost <strong>voor mij NIETS</strong> extra! <br /> Maar..."),
             new MarkupString("De web bedrijven betalen een percent! <strong>WAUW, hoor je dit?</strong>")
