541439e680b25d31a4f4d82e7382d50090da5bf5
Dennie Declercq <dennie@ddsoft.be>
2021-07-14 15:25:40 +0200

    Add project files.

---
+++ IkVerveelMij/Model/Content.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IkVerveelMij.Model
+{
+    public static class Content
+    {
+        private static readonly List<MarkupString> _content = new List<MarkupString>
+        {
+            new MarkupString("Ik heb tijd vrij..."),
+            new MarkupString("Wat zou ik doen?"),
+            new MarkupString("Laat ik een: <br /><ul><li>Niew boek</li><li>Nieuwe trui</li><li>Uitstapje</li><li>...</li></ul>leveren!<br/> <strong>Super</strong>! <br/> En als we terwijl aan <strong>DDSoft vzw</strong> denken..."),
+            new MarkupString("Dan bestellen we bij <strong>TROOPER!</strong> <br/> WANT..."),
+            new MarkupString("Het kost <strong>voor mij NIETS</strong> extra! <br /> Maar..."),
+            new MarkupString("De web bedrijven betalen een percent! <strong>WAUW, hoor je dit?</strong>")
+        };
+        public static  List<MarkupString> AllContent => _content;
+        public static  int Count => _content.Count;
+
+        public static MarkupString GetContent(int count)
+        {
+            return _content[count];
+        }
+    }
+}
---
+++ IkVerveelMij/Pages/Index.razor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IkVerveelMij.Model;
+using Microsoft.AspNetCore.Components;
+
+namespace IkVerveelMij.Pages
+{
+    public partial class Index
+    {
+        private int _count;
+        public MarkupString ContentToShow { get; set; }
+
+        public void  Next()
+        {
+            _count = _count < Content.Count-1 ? _count += 1 : 0;
+            ContentToShow = Content.GetContent(_count);
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            _count = 0;
+            ContentToShow = Content.GetContent(_count);
+        }
+    }
+}
