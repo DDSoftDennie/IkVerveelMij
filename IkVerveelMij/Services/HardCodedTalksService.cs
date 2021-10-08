6af6e0d9e0769fcbb774306010adc71241e26df8
Dennie Declercq <dennie@ddsoft.be>
2021-10-08 17:58:12 +0200

    Talks Added

--- IkVerveelMij/Services/HardCodedTalksService.cs
+++ IkVerveelMij/Services/HardCodedTalksService.cs
@@ -9,6 +9,20 @@ namespace IkVerveelMij.Services
     {
         private static List<Talk> talks = new List<Talk>()
         {
+            new Talk
+            {
+                Date = new DateTime(2021,10,21),
+                Title = "AI for the Bettter",
+                Event = new Event
+                {
+                    DateFrom = new DateTime(2021,10,21),
+                    DateTo = new DateTime(2021,10,21),
+                    isSpeaking = true,
+                    Name = "Global AI Bootcamp Pune, India",
+                    Location = "Virtual",
+                    Website = "https://globalai.community/global-ai-back-together/asia-global-ai-pune-4406/"
+                }
+            },
             new Talk{
                      Date = new DateTime(2021,10,29),
                      Title = "AI for the Better",
@@ -33,9 +47,36 @@ namespace IkVerveelMij.Services
                         Name = "Build Stuff",
                         Location = "Vilnius Lithuania",
                         Website = "https://www.buildstuff.events/travel-tips"
-
                     },
 
+            },
+            new Talk
+            {
+                Date = new DateTime(2021,11,19),
+                Title = "Blazing Accessibility Basics",
+                Event = new Event
+                {
+                    DateFrom = new DateTime(2021,11,18),
+                    DateTo = new DateTime(2021,11,19),
+                    isSpeaking = true,
+                    Name = "Update Conference Prague",
+                    Location = "Prague, Chech Republic",
+                    Website = "https://www.updateconference.net"
+                }
+            },
+            new Talk
+            {
+                Date = new DateTime(2021,12,15),
+                Title = "The Scary Truth about Labels",
+                Event = new Event
+                {
+                    DateFrom = new DateTime(2021,12,15),
+                    DateTo = new DateTime(2021,12,15),
+                    isSpeaking = true,
+                    Name = "Tech A11y Summit",
+                    Location = "Online",
+                    Website = "https://techa11y.dev"
+                }
             }
         };
 
