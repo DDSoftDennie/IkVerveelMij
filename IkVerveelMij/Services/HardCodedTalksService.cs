e7d5a3f6db6073b33784e4123f893e7ceaa9a322
Dennie Declercq <dennie@ddsoft.be>
2022-01-11 09:46:56 +0100

    talks updated!

--- IkVerveelMij/Services/HardCodedTalksService.cs
+++ IkVerveelMij/Services/HardCodedTalksService.cs
@@ -91,7 +91,64 @@ namespace IkVerveelMij.Services
                     Location = "Online",
                     Website = "https://techa11y.dev"
                 }
+            },
+            new Talk
+            {
+                Date = new DateTime(2022,1,15),
+                Title = "Speech Everything and (let them) Rise!",
+                Event = new Event
+                {
+                    DateFrom = new DateTime(2022,1,15),
+                    DateTo = new DateTime(2022,1,15),
+                    isSpeaking = true,
+                    Name = "Global AI Bootcamp Pune",
+                    Location = "Online",
+                    Website = "https://globalai.community/bootcamp-2022/asia-india-pune-4407/"
+                }
+            },
+            new Talk
+            {
+                Date = new DateTime(2022,1,17),
+                Title = "Autism in Tech",
+                Event = new Event
+                {
+                    DateFrom = new DateTime(2022,1,17),
+                    DateTo = new DateTime(2022,1,17),
+                    isSpeaking = true,
+                    Name = "Microsoft Reactor | Putting Accessibility into Practice Series",
+                    Location = "Online",
+                    Website = "https://www.meetup.com/Microsoft-Reactor-London/events/282821823/?utm_medium=email&utm_source=braze_canvas&utm_campaign=mmrk_alleng_event_announcement_prod_v5_en&utm_term=promo&utm_content=lp_meetup"
+                }
+            },
+            new Talk
+            {
+                Date = new DateTime(2022,3,16),
+                Title = "Exploring Patterns to Debug your Life (co speaker: Miriam Perrone)",
+                Event = new Event
+                {
+                    DateFrom = new DateTime(2022,3,15),
+                    DateTo = new DateTime(2022,3,17),
+                    isSpeaking = true,
+                    Name = "Javaland 2022",
+                    Location = "Phantasialand Bruhl Germany",
+                    Website = "https://www.javaland.eu"
+                }
+            },
+            new Talk
+            {
+                Date = new DateTime(2022,6,1),
+                Title = "Blazing Acccesibility Basics",
+                Event = new Event
+                {
+                    DateFrom = new DateTime(2022,6,1),
+                    DateTo = new DateTime(2022,6,1),
+                    isSpeaking = true,
+                    Name = "Scottish Summit",
+                    Location = "Glasgow, Scotland",
+                    Website = "https://scottishsummit.com"
+                }
             }
+
         };
 
 
